80f3687d43f35c09daec54c3f5e518a85fea0a61
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-01-27 15:01:47 +0000

    added a test for handling negative numbers

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,5 +1,9 @@
 const fizzBuzz = require("./fizzBuzz");
 
-test("Inputting a string give an error message.", () => {
+test("Inputting a string gives an error message.", () => {
   expect(fizzBuzz("hi")).toBe("Please input an integer value.");
+});
+
+test("Inputting a negative number gives an error message.", () => {
+  expect(fizzBuzz(-4)).toBe("Please input a value greater than 0.");
 });
