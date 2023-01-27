c317d3f11e9be9dc48a192e2cf651125bf4743a1
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-01-27 15:17:18 +0000

    added test for returning fizzbuzz string

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -7,3 +7,7 @@ test("Inputting a string gives an error message.", () => {
 test("Inputting a negative number gives an error message.", () => {
   expect(fizzBuzz(-4)).toBe("Please input a value greater than 0.");
 });
+
+test("Inputting a valid number returns a fizzbuzzified string.", () => {
+  expect(fizzBuzz(4)).toBe("1, 2, Fizz, 4");
+});
