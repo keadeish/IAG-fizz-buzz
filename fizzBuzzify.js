0416f657d2cc0fbb727f5eb86ba5469701f29077
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-01-29 20:48:26 +0000

    made the programme recursive, added comments

--- fizzBuzzify.js
+++ fizzBuzzify.js
@@ -1,18 +1,34 @@
 const fizzBuzz = require("./fizzBuzz");
 const readline = require("readline");
 
+// Create readline interface for user input
 const r1 = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-r1.question("Please enter a number to FizzBuzzify: ", (num) => {
-  console.log(typeof parseInt(num));
-  if (Number.isNaN(num)) {
-    console.log("Please enter a valid number.");
-  } else {
-    const result = fizzBuzz(parseFloat(num));
-    console.log(result);
-    r1.close();
-  }
-});
+// Recursive function to repeatedly ask for a number
+const askForNumber = () => {
+  r1.question(
+    "Please enter a number to FizzBuzzify or press q to quit: ",
+    (num) => {
+      // Parse user input as a number
+      const parsedNum = parseFloat(num);
+      // If input is "q", close readline interface
+      if (num === "q") {
+        return r1.close();
+      } else if (Number.isNaN(parsedNum)) {
+        // If input is not a number, log an error message
+        console.log("That was not a valid value!");
+      } else {
+        // If input is a number, call fizzBuzz function and log the result
+        const result = fizzBuzz(parsedNum);
+        console.log("Your result is:", result);
+      }
+      // Call the function again to ask for another number
+      askForNumber();
+    }
+  );
+};
+
+askForNumber();
