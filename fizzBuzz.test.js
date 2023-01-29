8dc549ef8a23422bf455a93aed567b88480ff87a
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-01-29 20:54:28 +0000

    added test with a bigger number, fixed error message outputs

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,13 +1,19 @@
 const fizzBuzz = require("./fizzBuzz");
 
 test("Inputting a string gives an error message.", () => {
-  expect(fizzBuzz("hi")).toBe("Please input an integer value.");
+  expect(fizzBuzz("hi")).toEqual("Please input a positive integer value.");
 });
 
 test("Inputting a negative number gives an error message.", () => {
-  expect(fizzBuzz(-4)).toBe("Please input a value greater than 0.");
+  expect(fizzBuzz(-4)).toEqual("Please input a positive integer value.");
 });
 
 test("Inputting a valid number returns a fizzbuzzified string.", () => {
-  expect(fizzBuzz(4)).toBe("1, 2, Fizz, 4");
+  expect(fizzBuzz(4)).toEqual("1, 2, Fizz, 4");
+});
+
+test("Input a number 15 or greater returns Fizz Buzz and FizzBuzz at the correct places", () => {
+  expect(fizzBuzz(16)).toEqual(
+    "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16"
+  );
 });
